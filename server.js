285689a94baa687c715c1a646747f32f448a9d7b
Zhijie Mo <alexmo@MacBook-Pro.local>
2020-11-04 17:11:29 -0500

    update resume

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ let serveStatic = require('serve-static');
 
 connect()
     .use(serveStatic(__dirname + '/static/'))
-    .listen(80, () => console.log('Server running on 80...'));
+    .listen(8080, () => console.log('Server running on 80...'));
